be8a6b6f7117773351389cf4ee598a9806e7595a
tyuttaporn <31217611+tyuttaporn@users.noreply.github.com>
2017-08-22 00:16:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,9 +3,10 @@
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
 
-if ($verify_token === 'abc123') {
+if ($verify_token === 'EAABytQT4K0EBAPKjR8dywmqEHdO8QWLgzgRIlqyABA2sevKMqV7195TAM1KZAQJGLbcyWxir9PbuKPWiKNcZAnNIZAdzmbIofPucybepaSIluzJFh0HZA2TiONfXWaf7fkjzzMX2ZAeWTyEBZCi9bEUZABF564fJxYThpjkcS2UqQZDZD') {
   echo $challenge;
 }
 
 $input = json_decode(file_get_contents('php://input'), true);
 error_log(print_r($input, true));
+?>
