97dab9f025e4e0a1af60f50541f2c3d15870e925
tyuttaporn <31217611+tyuttaporn@users.noreply.github.com>
2017-08-22 00:07:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&&
+  if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === 'EAABytQT4K0EBAPKjR8dywmqEHdO8QWLgzgRIlqyABA2sevKMqV7195TAM1KZAQJGLbcyWxir9PbuKPWiKNcZAnNIZAdzmbIofPucybepaSIluzJFh0HZA2TiONfXWaf7fkjzzMX2ZAeWTyEBZCi9bEUZABF564fJxYThpjkcS2UqQZDZD') {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
