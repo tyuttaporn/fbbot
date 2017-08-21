a9390e994fc69a78d6aa48a65592a086a3bc0db3
tyuttaporn <31217611+tyuttaporn@users.noreply.github.com>
2017-08-22 00:38:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-app.get('/webhook', function (req, res) {
+app.get('/app.js', function (req, res) {
     if (req.query['hub.verify_token'] === 'EAABytQT4K0EBAPKjR8dywmqEHdO8QWLgzgRIlqyABA2sevKMqV7195TAM1KZAQJGLbcyWxir9PbuKPWiKNcZAnNIZAdzmbIofPucybepaSIluzJFh0HZA2TiONfXWaf7fkjzzMX2ZAeWTyEBZCi9bEUZABF564fJxYThpjkcS2UqQZDZD') {
       res.send(req.query['hub.challenge']);
     } else {
